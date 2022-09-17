41ffed3427d70f0617bbd552b4d893beabc1d32e
Ali Fahed <alifahed67@gmail.com>
2022-09-18 02:59:05 +0800

    Add message form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message Board" });
+});
+
+router.post("/new", function (req, res, next) {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.username,
+    added: new Date(),
+  };
+
+  messages.unshift(newMessage);
+  res.redirect("/");
+});
+
 module.exports = router;
