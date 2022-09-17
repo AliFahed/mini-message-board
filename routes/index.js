4eeeb5c0201ba9a8a03c5abebea6d78943cc8924
Ali Fahed <alifahed67@gmail.com>
2022-09-18 00:18:22 +0800

    Add Pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hey Everyone",
+    user: "Ali",
+    added: new Date(),
+  },
+  {
+    text: "Hey",
+    user: "Draco",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
